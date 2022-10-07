27bf9fceed299e4a2443408808b945730419e70b
João Lee <joaumlimaum@gmail.com>
2022-10-07 19:14:30 -0300

    Mini mock para response criado

--- src/tests/integration/chocolates.test.js
+++ src/tests/integration/chocolates.test.js
@@ -12,5 +12,12 @@ describe('Usando o método GET em /chocolates', function () {
           .request(app)
           .get('/chocolates');
           expect(response.status).to.be.equals(200);
+      const output = [
+            { id: 1, name: 'Mint Intense', brandId: 1 },
+            { id: 2, name: 'White Coconut', brandId: 1 },
+            { id: 3, name: 'Mon Chéri', brandId: 2 },
+            { id: 4, name: 'Mounds', brandId: 3 },
+          ];
+          expect(response.body.chocolates).to.deep.equal(output);
   });
 });
