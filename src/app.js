3ebf6a7e6e313a683bbd545f53bbcbee1d78fd3f
João Lee <joaumlimaum@gmail.com>
2022-10-08 01:25:52 -0300

    Rota GET /chocolates/:id funcional e com teste passando

--- src/app.js
+++ src/app.js
@@ -8,13 +8,20 @@ app.get('/chocolates', async (_req, resp) => {
   resp.status(200).json({ allChocolates });
 });
 
+app.get('/chocolates/:id', async (req, resp) => {
+  const { id } = req.params;
+  const chocolateById = await getAllChocolates();
+  const chocolates = await chocolateById.find((chocolate) => chocolate.id === Number(id));
+  resp.status(200).json({ chocolates });
+});
+
 // app.get('/chocolates/:id', async (req, resp) => {
 //   const { id } = req.params;
 //   const chocolateById = await getAllChocolates();
 //   const chocolates = chocolateById.find((chocolate) => chocolate.id === Number(id));
 //   resp.status(200).json({ chocolates });
 // });
-
 module.exports = {
   app
 };
+
--- src/tests/integration/chocolates.test.js
+++ src/tests/integration/chocolates.test.js
@@ -6,18 +6,26 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const output = [
+  { id: 1, name: 'Mint Intense', brandId: 1 },
+  { id: 2, name: 'White Coconut', brandId: 1 },
+  { id: 3, name: 'Mon Chéri', brandId: 2 },
+  { id: 4, name: 'Mounds', brandId: 3 },
+];
+
 describe('Usando o método GET em /chocolates', function () {
   it('Retorna a lista completa de chocolates!', async function () {
     const response = await chai
       .request(app)
       .get('/chocolates');
     expect(response.status).to.be.equals(200);
-    const output = [
-      { id: 1, name: 'Mint Intense', brandId: 1 },
-      { id: 2, name: 'White Coconut', brandId: 1 },
-      { id: 3, name: 'Mon Chéri', brandId: 2 },
-      { id: 4, name: 'Mounds', brandId: 3 },
-    ];
     expect(response.body.allChocolates).to.deep.equal(output);
+  });
+});
+describe('Usando o método GET em /chocolates/:id', function () {
+  it('Retorna chocolates pelo id', async function () {
+    const response = await chai.request(app).get('/chocolates/1')
+    expect(response.status).to.be.equals(200);
+    expect(response.body.chocolates).to.deep.equal(output[0]);
   });
 });
