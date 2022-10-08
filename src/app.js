df0b9c410f75dab1884ba183def8fb6a58a1e97a
João Lee <joaumlimaum@gmail.com>
2022-10-07 23:05:11 -0300

    Criação do cacaoTrybe.js para fazer a leitura do arquivo json

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const cacaoTrybe = require('./data/cacaoTrybefile')
+const { getAllChocolates } = require('./cacaoTrybe');
+
+// const cacaoTrybe = require('./data/cacaoTrybefile');
+
 const app = express();
 
-app.get('/chocolates', (_req, resp) => {
-  resp.status(200).json(cacaoTrybe);
-})
+app.get('/chocolates', async (_req, resp) => {
+  const allChocolates = await getAllChocolates()
+  resp.status(200).json({ allChocolates });
+});
+
+// app.get('/chocolates/:id', (req, resp) => {
+//   const { id } = req.params;
+//   const convertionToObject = JSON.parse(cacaoTrybe);
+//   const { chocolates } = convertionToObject;
+//   chocolates.find((chocolate) => chocolate.id === Number(id))
+//   resp.status(200).json({ chocolates });
+// });
 
 module.exports = {
   app
-}
+};
