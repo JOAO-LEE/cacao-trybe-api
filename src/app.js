ad62f0e3d4f5aefcd3475db74d38dbca9402c591
João Lee <joaumlimaum@gmail.com>
2022-10-07 22:11:36 -0300

    Rota GET '/chocolates' criada

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
+const cacaoTrybe = require('./data/cacaoTrybefile')
 const app = express();
+
+app.get('/chocolates', (_req, resp) => {
+  resp.status(200).json(cacaoTrybe);
+})
 
 module.exports = {
   app
